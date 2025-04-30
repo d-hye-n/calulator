1582b2419394cab055ac6b183b3aab91c59b551c
d-hye-n <qordkadkwla@gmail.com>
2025-04-30 15:25:33 +0900

    Initial Commit

---
+++ calulatorTests/calulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  calulatorTests.swift
+//  calulatorTests
+//
+//  Created by 김도현 on 4/30/25.
+//
+
+import Testing
+@testable import calulator
+
+struct calulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
